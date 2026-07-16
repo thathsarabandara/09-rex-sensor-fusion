829c00394b680408d039bacee2fbb57b301a1705
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-07-16 17:21:40 +0530

    feat: add fusion module to the sensor fusion service:   init

---
+++ app/fusion/__init__.py
@@ -0,0 +1,5 @@
+from .obstacle import estimate_obstacle_risk
+from .motion import get_motion_state, detect_possible_stall
+from .orientation import is_tilt_unsafe, detect_possible_impact
+from .health import evaluate_sensor_health
+from .confidence import calculate_confidence
